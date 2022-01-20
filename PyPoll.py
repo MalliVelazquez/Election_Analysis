b1d77617100f12e01784055c8305e1c78319aafe
MalliVelazquez <96633294+MalliVelazquez@users.noreply.github.com>
2022-01-19 19:39:00 -0600

    adding updates

---
+++ PyPoll.py
@@ -0,0 +1,9 @@
+#Data we need to retreive
+file_variable = open(election_results.csv, r)
+
+#Total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#total number of votes each candidate won
+#Winner of the election
+
